4d82ca171e3d201720973c507a2f681a23851be9
muhammad farhan <muhammad.2842@students.amikom.ac.id>
2023-07-15 18:04:34 +0700

    Tugas 08

---
+++ Tugas Inheritance/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tugas_Inheritance
+{
+    class Person
+    {
+        public string Name = "";
+        public string Pekerjaan = "";
+        public int Age = 0;
+
+        public void InfoPerson(string Person)
+        {
+            Console.WriteLine("Nama saya  " + Name);
+            Console.WriteLine("Saya berumur " + Age);
+            Console.WriteLine("Saya bekerja sebagai " + Pekerjaan);
+            Console.WriteLine("");
+        }
+
+        public class infoPerson2 : Person
+        {
+            public void InfoPerson2(string Person2)
+            {
+                Console.WriteLine("Nama saya " + Name);
+                Console.WriteLine("Saya berumur " + Age);
+                Console.WriteLine("Saya bekerja sebagai " + Person2);
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ Tugas Inheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tugas_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namePerson = { "Farhan", "SheilaOn7" };
+            string[] karirPerson = { "Programer", "Musisi" };
+            int[] AgePerson = { 23, 21 };
+
+            Person PersonData = new Person();
+            PersonData.Name = namePerson[0];
+            PersonData.Age = AgePerson[0];
+            PersonData.InfoPerson(karirPerson[0]);
+
+            PersonData.Name = namePerson[1];
+            PersonData.Age = AgePerson[1];
+            PersonData.InfoPerson(karirPerson[1]);
+        }
+    }
+}
